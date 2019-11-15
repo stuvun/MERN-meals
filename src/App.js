719c0ea3b4ed979b9b35017a8ad85550dba66446
Steven Kim <sskim14@gmail.com>
2019-11-15 11:40:27 -0500

    Added fetch to meals api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      meals: ""
+    }
+  }
+  componentDidMount() {
+    fetch("https://stuvun-meals-api.herokuapp.com/")
+      .then(res => res.json())
+      .then(res => {
+        console.log(res, "Success");
+        this.setState({ meals: res })
+      })
+      .catch(err => {
+        console.log(err, "Something's wrong")
+      })
+  }
   render() {
+    let meals = this.state.meals
     return (
-      
+      <h1>{ meals }</h1>
     )
   }
 }
