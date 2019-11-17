3ad184158bb8d75ab77c645e2da4d1a7e9e52186
Steven Kim <sskim14@gmail.com>
2019-11-17 18:57:19 -0500

    Created Meal Cards for each meal in database

--- src/App.js
+++ src/App.js
@@ -25,18 +25,68 @@ class App extends Component {
 
     return (
       <div className="App">
-        <div className="meal">
+        <div className="mealContainer">
           { meals.map(item => {
             console.log(item.meal)
-            return(
-              <Meal
+            return (
+              <Meal className="meal"
               key={ item.meal[0]._id }
-              name={ item.meal[0].mealName }
-              image={ item.meal[0].image }
               id={ item.meal[0].id }
-            />
+              name={ item.meal[0].mealName }
+              category={ item.meal[0].category }
+              area={ item.meal[0].area }
+              instructions={ item.meal[0].instructions }
+              image={ item.meal[0].image }
+              tags={ item.meal[0].tags }
+              video={ item.meal[0].video }
+              ingredients={ [
+                item.meal[0].ingredient1,
+                item.meal[0].ingredient2,
+                item.meal[0].ingredient3,
+                item.meal[0].ingredient4,
+                item.meal[0].ingredient5,
+                item.meal[0].ingredient6,
+                item.meal[0].ingredient7,
+                item.meal[0].ingredient8,
+                item.meal[0].ingredient9,
+                item.meal[0].ingredient10,
+                item.meal[0].ingredient11,
+                item.meal[0].ingredient12,
+                item.meal[0].ingredient13,
+                item.meal[0].ingredient14,
+                item.meal[0].ingredient15,
+                item.meal[0].ingredient16,
+                item.meal[0].ingredient17,
+                item.meal[0].ingredient18,
+                item.meal[0].ingredient19,
+                item.meal[0].ingredient20
+              ] }
+              measurements={ [
+                item.meal[0].measurement1,
+                item.meal[0].measurement2,
+                item.meal[0].measurement3,
+                item.meal[0].measurement4,
+                item.meal[0].measurement5,
+                item.meal[0].measurement6,
+                item.meal[0].measurement7,
+                item.meal[0].measurement8,
+                item.meal[0].measurement9,
+                item.meal[0].measurement10,
+                item.meal[0].measurement11,
+                item.meal[0].measurement12,
+                item.meal[0].measurement13,
+                item.meal[0].measurement14,
+                item.meal[0].measurement15,
+                item.meal[0].measurement16,
+                item.meal[0].measurement17,
+                item.meal[0].measurement18,
+                item.meal[0].measurement19,
+                item.meal[0].measurement20
+              ] }
+              source={ item.meal[0].source }
+              dateModified={ item.meal[0].dateModified }
+              />
             )
-            
           }) }
         </div>
       </div>
--- src/components/Meal.js
+++ src/components/Meal.js
@@ -12,7 +12,34 @@ class Meal extends Component {
         return (
             <div className="meal">
                 <h1>{ this.props.name }</h1>
-                <img src={ this.props.image } alt={ this.props.id }/>
+                <div className="mealCard">
+                    <img className="mealImage"
+                        src={ this.props.image }
+                        alt={ this.props.id } />
+                    <div className="ingredients">
+                        <h2>Ingredients</h2>
+                        <div className="measureIn">
+                            <div className="listContainer">
+                                <ul className="measurementList">
+                                    { this.props.measurements.map(measurement => {
+                                        if (measurement) {
+                                            return ( <li className="measurementItem">{ measurement }</li> )
+                                        }
+                                    })}
+                                </ul>
+                            </div>
+                            <div className="listContainer">
+                                <ul className="ingredientList">
+                                    { this.props.ingredients.map(ingredient => {
+                                        if (ingredient) {
+                                            return ( <li className="ingredientItem">{ ingredient }</li> )
+                                        }
+                                    })}
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
